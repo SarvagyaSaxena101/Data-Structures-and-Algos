2bd636f1cca44dbc2b3e827d2a92b4e3a8cba1e5
SarvagyaSaxena101 <sarvagya.saxena_cs.h23@gla.ac.in>
2025-01-14 15:19:16 +0530

    Last commit of today

---
+++ Binary_Search.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Binary_Search {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int key = sc.nextInt();
+        int start = 0;
+        int end = arr.length-1;
+        while (start<= end) {
+            int mid = (start+end)/2;
+            if (arr[mid] == key) {System.out.println(mid); break;}
+            else if(arr[mid] > key) end = mid-1;
+            else start = mid + 1;
+        }
+
+    }
+    
+}
