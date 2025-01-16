905367b7408275247540e2a4af88b52033a49ba3
SarvagyaSaxena101 <sarvagya.saxena_cs.h23@gla.ac.in>
2025-01-16 11:11:04 +0530

    git commit

--- recursion_practice/factorial_recursion.java
+++ recursion_practice/factorial_recursion.java
@@ -2,7 +2,7 @@ package recursion_practice;
 
 public class factorial_recursion {
     public static int factorial(int i){
-        if(i == 0) return 1;
+        if(i == 1) return 1;
         return i * factorial(i-1);
     }
     public static void main(String[] args) {
