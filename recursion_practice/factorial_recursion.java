250d578252c6b52329258501f170a51f928e2cd3
SarvagyaSaxena101 <sarvagya.saxena_cs.h23@gla.ac.in>
2025-01-16 11:09:46 +0530

    factorial code using recurssion

---
+++ recursion_practice/factorial_recursion.java
@@ -0,0 +1,12 @@
+package recursion_practice;
+
+public class factorial_recursion {
+    public static int factorial(int i){
+        if(i == 0) return 1;
+        return i * factorial(i-1);
+    }
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+    }
+    
+}
